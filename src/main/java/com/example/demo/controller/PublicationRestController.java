f8c6d0d2d0ccb19319f0bc029483cdb0133140e8
dev <dbs.aymen@gmail.com>
2020-01-13 12:34:54 +0100

    add find by date between to pubs and events

--- src/main/java/com/example/demo/controller/PublicationRestController.java
+++ src/main/java/com/example/demo/controller/PublicationRestController.java
@@ -4,22 +4,33 @@ import com.example.demo.entity.Publication;
 import com.example.demo.services.IPublicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/pulication")
+@RequestMapping(value = "/publication")
 @CrossOrigin
 public class PublicationRestController {
     @Autowired
     IPublicationService publicationService;
 
-    @RequestMapping(value="/all",method= RequestMethod.GET)
+    @GetMapping(value="/all")
     public List<Publication> findAllPuvlications(){
         return publicationService.findAllPub();
+    }
+    @GetMapping(value="/search/findByDateBetween/{startDate}/{endDate}")
+    public List<Publication> findByDateBetween(@PathVariable("startDate") String startDate,
+                                               @PathVariable("endDate") String endDate) throws Exception {
+        try {
+            Date startdate = new SimpleDateFormat("dd-MM-yyyy").parse(startDate);
+            Date enddate = new SimpleDateFormat("dd-MM-yyyy").parse(endDate);
+            return publicationService.findPubByDateBetween(startdate, enddate);
+        } catch (ParseException e) {
+            throw new Exception("Invalid Date Format, Date Format should be like: dd-mm-yyyy");
+        }
     }
 }
